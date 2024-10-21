12f2f4eaecb8a480203b244a9b941d83eaf2fef1
shine26 <chews26@naver.com>
2024-10-21 20:15:34 +0900

    :art: 추가 : 배열 수정 및 게임 로직 구성

--- src/Lv1WordleGame/BaseballGame.java
+++ src/Lv1WordleGame/BaseballGame.java
@@ -1,14 +1,12 @@
 package Lv1WordleGame;
 
-import java.util.LinkedHashSet;
-import java.util.Random;
+import java.util.*;
 
 public class BaseballGame {
-    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    private final LinkedHashSet<Integer> inputHashSet = new LinkedHashSet<>();
+    private final LinkedHashSet<Integer> randomHashSet = new LinkedHashSet<>();
 
-    }
-    private LinkedHashSet<Integer> inputArr = new LinkedHashSet<Integer>();
-    private LinkedHashSet<Integer> randomAnswerArr = new LinkedHashSet<Integer>();
 
     // 객체 생성시 정답을 만들도록 함 (생성자)
     public BaseballGame() {
@@ -19,48 +17,89 @@ public class BaseballGame {
     public void makeRandomAnswer() {
         Random random = new Random();
 
-        for (int i = 0; randomAnswerArr.size() < 3; i++) {
+        for (int i = 0; randomHashSet.size() < 3; i++) {
             int randomNumber = random.nextInt(9) + 1;
-            randomAnswerArr.add(randomNumber);
+            randomHashSet.add(randomNumber);
         }
 
-        System.out.println(randomAnswerArr);
+        System.out.println("디버그 용 : " + randomHashSet); // 디버그용
     }
 
+    public void play() {
+        while (true) {
 
-    public int play(int number) {
-        boolean checkInput = false;
+            // 1. 유저에게 입력값을 받음
+            System.out.println("세자리 숫자를 입력하세요.");
+            int number = sc.nextInt();
 
-        while (!checkInput) {
             // 배열에 저장하기 위해 int -> String으로 변환
             String inputString = String.valueOf(number);
 
-
             // 배열에 저장
             for (int i = 0; i < 3; i++) {
-                inputArr.add(inputString.charAt(i) - '0');
+                inputHashSet.add(inputString.charAt(i) - '0');
             }
 
-            System.out.println(inputArr);
-//            return 0;
+            ArrayList<Integer> randomArrayList = new ArrayList<Integer>(randomHashSet);
+            System.out.println("디버그 용 : " + randomArrayList); //디버그 용
+
+
+            ArrayList<Integer> inputArrayList = new ArrayList<Integer>(inputHashSet);
+            System.out.println("디버그 용 : " + inputHashSet); //디버그 용
+
 
             // 2. 올바른 입력값을 받았는지 검증
-            if (inputArr.contains(0)) {
-                System.out.println("0은 입력될 수 없습니다.");
+            // 3. 게임 진행횟수 증가
+
+            // 5. 정답여부 확인, 만약 정답이면 break 를 이용해 반복문 탈출
+            if (Arrays.equals(randomArrayList.toArray(), inputArrayList.toArray())) {
+                System.out.println("축하합니다 정답입니다!!!");
+                break;
             }
 
-            // 3. 게임 진행횟수 증가
+
             // 4. 스트라이크 개수 계산
-            // 5. 정답여부 확인, 만약 정답이면 break 를 이용해 반복문 탈출
+            int strikeCount = 0;
+            int ballCount = 0;
+            for (int i = 0; i < 3; i++) {
+                if (randomArrayList.get(i).equals(inputArrayList.get(i))) {
+                    strikeCount++;
+                } else if (randomArrayList.contains(inputArrayList.get(i))) {
+                    ballCount++;
+                }
+            }
+            System.out.println(strikeCount + " 스트라이크");
+            System.out.println(ballCount + " 볼");
+
 
             // 6. 볼 개수 계산
+//            int ballCount = 0;
+//            for (int i = 0; i < 3; i++) {
+//                if (randomArrayList.contains(inputArrayList.get(i))) {
+//                    ballCount++;
+//                }
+//            }
+//            System.out.println(ballCount + " 볼");
+
             // 7. 힌트 출력
 
-
+            // 입력 인덱스 값 초기화
+                inputHashSet.clear();
+                inputArrayList.clear();
+                System.out.println("---------------------------");
+            }
         }
+
+
+    public HashSet<Integer> getInputHashSet() {
+        return inputHashSet;
+    }
+
+    public HashSet<Integer> getRandomAnswerArr() {
+        return randomHashSet;
     }
 }
-        // 게임 진행횟수 반환
+// 게임 진행횟수 반환
 //    }
 //
 //    protected boolean validateInput(String input) {
--- src/Lv1WordleGame/Main.java
+++ src/Lv1WordleGame/Main.java
@@ -1,9 +1,18 @@
+package Lv1WordleGame;
+
+import java.util.Scanner;
+
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
+        BaseballGame baseballGamest = new BaseballGame();
 
+        Scanner sc = new Scanner(System.in);
+        // 1. 유저에게 입력값을 받기
 
+        System.out.println("<<<<Wordle 게임을 시작합니다.>>>>");
+        baseballGamest.play();
     }
 }
 
